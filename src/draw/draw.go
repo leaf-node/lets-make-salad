0246cd42ccb7006b88a42d5b17947b2430bde7b7
Andrew Engelbrecht <andrew@sourceflow.dev>
2023-02-18 17:42:27 -0500

    add more textures, minor refactoring

--- src/draw/draw.go
+++ src/draw/draw.go
@@ -13,8 +13,10 @@ var spriteSize = int32(16)
 
 type assets struct {
     rock rl.Texture2D
+    stones rl.Texture2D
     grass rl.Texture2D
     swamp rl.Texture2D
+    dirt rl.Texture2D
 }
 
 func Init() assets {
@@ -30,26 +32,33 @@ func Init() assets {
 
 func Draw(world *game.World, as assets) {
 
-    //world.Tiles.DebugPrintMap()
-
     rl.BeginDrawing()
     rl.ClearBackground(rl.Black)
 
     height := int32(world.Tiles.Height)
     width := int32(world.Tiles.Width)
 
+    tint := rl.White
+
     for x := int32(0); x < width ; x++ {
         for y := int32(0); y < height ; y++ {
 
             tile := world.Tiles.GetTile(int(x), int(y))
 
+            x1 := x * spriteSize
+            y1 := y * spriteSize
+
             switch tile {
             case "R":
-                rl.DrawTexture(as.rock, x * spriteSize, y * spriteSize, rl.White)
+                rl.DrawTexture(as.rock, x1, y1, tint)
+            case "r":
+                rl.DrawTexture(as.stones, x1, y1, tint)
+            case ".":
+                rl.DrawTexture(as.grass, x1, y1, tint)
             case ":":
-                rl.DrawTexture(as.swamp, x * spriteSize, y * spriteSize, rl.White)
+                rl.DrawTexture(as.swamp, x1, y1, tint)
             default:
-                rl.DrawTexture(as.grass, x * spriteSize, y * spriteSize, rl.White)
+                rl.DrawTexture(as.dirt, x1, y1, tint)
             }
         }
     }
@@ -60,14 +69,18 @@ func Draw(world *game.World, as assets) {
 func (as *assets) load() {
 
     as.rock = rl.LoadTexture("img/rock.png")
+    as.stones = rl.LoadTexture("img/stones.png")
     as.grass = rl.LoadTexture("img/grass.png")
     as.swamp = rl.LoadTexture("img/swamp.png")
+    as.dirt = rl.LoadTexture("img/dirt.png")
 }
 
 func (as *assets) unload() {
 
     rl.UnloadTexture(as.rock)
+    rl.UnloadTexture(as.stones)
     rl.UnloadTexture(as.grass)
     rl.UnloadTexture(as.swamp)
+    rl.UnloadTexture(as.dirt)
 }
 
