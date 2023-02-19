52288b0f9e3f7532da24e0cc6a753b1f68c0dfc1
Andrew Engelbrecht <andrew@sourceflow.dev>
2023-02-19 18:36:11 -0500

    add random stone or wood in demo

--- src/game/game.go
+++ src/game/game.go
@@ -27,13 +27,21 @@ func Init (seed string, gridSize int32, noiseScale float64) *World {
 
 func (world *World) Update() {
 
+    var item string
+
     x := int32(rand.Intn(int(world.Tiles.Width)))
     y := int32(rand.Intn(int(world.Tiles.Height)))
+
+    if rand.Intn(2) == 1 {
+        item = "w"
+    } else {
+        item = "s"
+    }
 
     switch world.Tiles.GetTile(x, y) {
     case ".":
         if world.Items.GetItem(x, y) == "" {
-            world.Items.AddItem("w", x, y)
+            world.Items.AddItem(item, x, y)
         }
     }
 
